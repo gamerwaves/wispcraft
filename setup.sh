79636db20e06f7530a36b512c84c1a5ca039e464
Percs <83934299+Percslol@users.noreply.github.com>
2025-05-13 11:00:19 -0500

    fix static build

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ cd dist
 wget https://bafybeidtmtattvhd2y45dbafncqmu52bxjr3kwyrftky552bpnomy2vzpu.ipfs.dweb.link/?filename=EaglercraftX_1.8_WASM-GC_Offline_Download.zip
 unzip EaglercraftX_1.8_WASM-GC_Offline_Download.zip
 rm EaglercraftX_1.8_WASM-GC_Offline_Download.zip
-sed -i 's/<head>/<head>\<script src="index.js"><\/script>/' EaglercraftX_1.8_WASM-GC_Offline_Download.html
+mv EaglercraftX_1.8_WASM-GC_Offline_Download.html index.html
+sed -i 's/<head>/<head>\<script src="index.js"><\/script>/' index.html
 cp ../ci/_headers .
